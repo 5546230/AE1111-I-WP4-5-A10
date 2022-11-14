c88a0906c575aa9a253aadf6cd628bc9909da7fa
5546230 <n.g.j.schotmeijer@student.tudelft.nl>
2022-11-14 16:18:28 +0100

    aerodynic forces span

--- Code/interpolation.py
+++ Code/interpolation.py
@@ -12,9 +12,7 @@ ylst, clst, ailst, Cllst, Cdilst, Cmlst = get_lst(filename)
 v= 10#[m/s]
 rho = 1.225#[m/s}
 
-Lspanlst = 0.5*v**2*rho*Cllst*clst
-
-#interplotion functions
+#interpolation functions
 get_c = sp.interpolate.interp1d(ylst, clst, kind="linear", fill_value="extrapolate")
 
 get_cl = sp.interpolate.interp1d(ylst, Cllst, kind="cubic", fill_value="extrapolate")
@@ -23,8 +21,15 @@ get_cdi = sp.interpolate.interp1d(ylst, Cdilst, kind="cubic", fill_value="extrap
 
 get_cm = sp.interpolate.interp1d(ylst, Cmlst, kind="cubic", fill_value="extrapolate")
 
-get_Lspan = sp.interpolate.interp1d(ylst, Lspanlst, kind="cubic", fill_value="extrapolate")
+def get_Lspan(y):
+    return 0.5*rho*v**2*get_cl(y)*get_c(y)
 
+def get_cmspan(y):
+    return 0.5*rho*v**2*get_cl(y)*get_c(y)**2
+
+def get_cdispan(y):
+    return 0.5*rho*v**2*get_cl(y)*get_c(y)
+    
 # y = np.linspace(0.5,12,100)
 # Lspan = get_Lspan(y)
 # plt.plot(y,Lspan)
