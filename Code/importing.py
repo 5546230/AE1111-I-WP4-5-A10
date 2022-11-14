3603e648f98bda4cfe5a2ce46200399d47df9257
5546230 <n.g.j.schotmeijer@student.tudelft.nl>
2022-11-14 15:09:48 +0100

    Importing data
    the data can now be imported

--- Code/importing.py
+++ Code/importing.py
@@ -1,14 +1,17 @@
 import numpy as np
+from scipy import interpolate
 
-def get_lst(filename: str)->np.ndarray:
+def get_lst(filename: str) -> np.ndarray:
     '''import the data file'''
     #get a numpy array of the data with the header and footer skipped
     lst = np.genfromtxt(
         filename, 
-        skip_footer=True, 
-        skip_header=True
+        delimiter=",",
+        skip_header= 21,
+        skip_footer=2523
     )
 
+    print(lst)
     #store all paramters in it's own lists
     ylst = lst[0]
     clst = lst[1]
@@ -19,3 +22,5 @@ def get_lst(filename: str)->np.ndarray:
 
     #return the lists
     return ylst, clst, ailst, Cllst, Cdilst, Cmlst
+
+ylst, clst, ailst, Cllst, Cdilst, Cmlst = get_lst("Data\MainWing_a=0.00_v=10.00ms.csv")
