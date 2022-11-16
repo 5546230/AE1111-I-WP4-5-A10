0e81ca31520fbe8bbb350ae69e9951baa12a0f81
CristianPetria <M.C.Petria@student.tudelft.nl>
2022-11-16 17:25:13 +0100

    Merge branch 'main' of https://github.com/5546230/AE1111-I-WP4-5-A10

--- Code/Weight_diagram.py
+++ Code/Weight_diagram.py
@@ -3,6 +3,10 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 t= 0.01
+A_root = 1.094
+mass = 756
+lamda = 0.372
+volume = 6.613 
 def spar_weight_025(y):
     w = 2700*0.114*3.5*(1-(1-0.372)*(y/11.98))*9.81*t
     return w 
@@ -17,7 +21,7 @@ def fuel_weight(y):
 
 
 def get_Weight(y):
-    return spar_weight_025(y)+spar_weight_075(y)+ fuel_weight(y)
+    return A_root*((lamda-1)/12*y+1)**2*mass/volume   
 
 def main():
     y_axis = np.linspace(0,12,1000)
