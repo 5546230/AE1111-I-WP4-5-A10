4f731b83cc6f732730201e134d811872a68272d8
5546230 <n.g.j.schotmeijer@student.tudelft.nl>
2022-11-16 17:01:35 +0100

    wieght

--- Code/Lift_diagram.py
+++ Code/Lift_diagram.py
@@ -1,10 +1,12 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from interpolation import get_Lspan
+import scipy as sp
 
 y_axis = np.linspace(0.5,11.98, 1000)
 L_span = get_Lspan(y_axis,10/180*np.pi,10,1.225)
-
+print(sp.integrate.quad(get_Lspan, 0, 11.98, args=(0, 200, 0.287446962
+)))
 plt.plot(y_axis, L_span)
 plt.title("Spanwise lift distribution")
 plt.ylabel("Lift [N]")
--- Code/Weight_diagram.py
+++ Code/Weight_diagram.py
@@ -2,21 +2,23 @@ import scipy as sp
 import numpy as np
 from matplotlib import pyplot as plt
 
+t= 0.01
 def spar_weight_025(y):
-    w = 2700*0.114*3.5*(1-(1-0.372)(y/11.98))
-    return
+    w = 2700*0.114*3.5*(1-(1-0.372)*(y/11.98))*9.81*t
+    return w 
 
 def spar_weight_075(y):
-    return
+    w = 2700*0.063*3.5*(1-(1-0.372)*(y/11.98))*9.81*t
+    return w 
 
 def get_Weight(y):
-    return y 
+    return spar_weight_025(y)+spar_weight_075(y) 
 
 def main():
     y_axis = np.linspace(0,12,1000)
     Weight=get_Weight(y_axis)
 
-
+    print(sp.integrate.quad(get_Weight, 0, 11.98))
     plt.plot(y_axis, Weight)
     plt.title("Spanwise Weight distribution")
     plt.xlabel("Spanwise location [m]")
@@ -25,3 +27,4 @@ def main():
 
 if __name__=="__main__":
     main()
+    
