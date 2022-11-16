b8cf542664ac32b8f5fb2157ef865df66be0b9bc
CristianPetria <M.C.Petria@student.tudelft.nl>
2022-11-16 14:23:27 +0100

    Changing of names of files and weight diagram

---
+++ Code/Shear_diagram.py
@@ -0,0 +1,8 @@
+from Weight_diagram import Weight 
+from Lift diagram import L_span
+
+
+def get_resultant(y):
+    return L_span(y)-Weight(y)
+
+
---
+++ Code/Weight_diagram.py
@@ -0,0 +1,16 @@
+import scipy as sp
+import numpy as np
+from matplotlib import pyplot as plt
+
+def get_Weight(y):
+    return y 
+
+y_axis = np.linspace(0,12,1000)
+Weight=get_Weight(y_axis)
+
+
+plt.plot(y_axis, Weight)
+plt.title("Spanwise Weight distribution")
+plt.xlabel("Spanwise location [m]")
+plt.ylabel("Weight [N]")
+
