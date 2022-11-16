de1f9d83144ea0a9b07cd6bb409cf33d92b339e8
moeb8001 <moh.ebroh@gmail.com>
2022-11-16 15:36:25 +0100

    works agian

--- Code/Lift_diagram.py
+++ Code/Lift_diagram.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 from interpolation import get_Lspan
 
 y_axis = np.linspace(0.5,11.98, 1000)
-L_span = get_Lspan(y_axis)
+L_span = get_Lspan(y_axis,10/180*np.pi,10,1.225)
 
 plt.plot(y_axis, L_span)
 plt.title("Spanwise lift distribution")
--- Code/Shear_diagram.py
+++ Code/Shear_diagram.py
@@ -1,4 +1,4 @@
-from Weight_diagram import Weight
+from Weight_diagram import get_Weight
 from Lift_diagram import L_span
 import numpy as np
 
@@ -7,7 +7,7 @@ y_axis = np.linspace(0.5,11.98, 1000)
 
 
 def get_resultant(y):
-    return L_span(y)+Weight(y)
+    return L_span(y,10/180*np.pi,10,1.225)+get_Weight(y)
 
 x_axis = np.linspace(0.5,11.98, 1000)
 
