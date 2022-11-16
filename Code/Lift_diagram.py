2ec14b998abf1611b9f6f27f2f022dfe8f4df725
moeb8001 <moh.ebroh@gmail.com>
2022-11-16 15:31:30 +0100

    diagra,s

--- Code/Lift_diagram.py
+++ Code/Lift_diagram.py
@@ -2,7 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from interpolation import get_Lspan
 
-y_axis = np.linspace(0,11.98, 100)
+y_axis = np.linspace(0.5,11.98, 1000)
 L_span = get_Lspan(y_axis)
 
 plt.plot(y_axis, L_span)
--- Code/Shear_diagram.py
+++ Code/Shear_diagram.py
@@ -1,8 +1,21 @@
-from Weight_diagram import Weight 
-from Lift diagram import L_span
+from Weight_diagram import Weight
+from Lift_diagram import L_span
+import numpy as np
+
+
+y_axis = np.linspace(0.5,11.98, 1000)
 
 
 def get_resultant(y):
-    return L_span(y)-Weight(y)
+    return L_span(y)+Weight(y)
+
+x_axis = np.linspace(0.5,11.98, 1000)
 
 
+#plt.plot(y_axis,x_axis)# get_resultant(y_axis))
+#plt.title("bla lift distribution")
+#plt.ylabel("bla [N]")
+#plt.xlabel("bla location [m]")
+#plt.show()
+
+print("hihhh")
