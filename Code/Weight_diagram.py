01b7077dd28e4bddc8f1eabd7e60b29ca6a5f83b
5546230 <n.g.j.schotmeijer@student.tudelft.nl>
2022-11-16 15:29:31 +0100

    dont run on import

--- Code/Weight_diagram.py
+++ Code/Weight_diagram.py
@@ -5,13 +5,16 @@ from matplotlib import pyplot as plt
 def get_Weight(y):
     return y 
 
-y_axis = np.linspace(0,12,1000)
-Weight=get_Weight(y_axis)
+def main():
+    y_axis = np.linspace(0,12,1000)
+    Weight=get_Weight(y_axis)
 
 
-plt.plot(y_axis, Weight)
-plt.title("Spanwise Weight distribution")
-plt.xlabel("Spanwise location [m]")
-plt.ylabel("Weight [N]")
-plt.show()
+    plt.plot(y_axis, Weight)
+    plt.title("Spanwise Weight distribution")
+    plt.xlabel("Spanwise location [m]")
+    plt.ylabel("Weight [N]")
+    plt.show()
 
+if __name__="__main__":
+    main()
