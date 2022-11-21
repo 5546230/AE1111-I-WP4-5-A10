ba461244f7fbb07f8ec1a87b89408d4245af55a3
5546230 <n.g.j.schotmeijer@student.tudelft.nl>
2022-11-21 13:37:05 +0100

    shear functions

--- Code/load_case.py
+++ Code/load_case.py
@@ -4,12 +4,11 @@ from Torque_diagram import torque_diagram
 from Weight_diagram import get_Weight
 import numpy as np
 import scipy as sp
+from scipy import interpolate
 from matplotlib import pyplot as plt
 
 
 class LoadCase:
-
-
     def __init__(self, n: float, W: float, v: float, h: float) -> None:
         self.n = n
         self.w = W
@@ -22,6 +21,10 @@ class LoadCase:
         self.rho = ISA_rho(self.h)
         self.cld = self.l/(0.5*self.rho*self.v**2*self.s)
         self.alpha = get_alpha(self.cld)
+
+        #get shear functions
+        self.z2_shear_calc()
+        self.x2_shear_calc()
     
     def z2_force(self, y: float) -> float:
         return (get_Lspan(y, self.alpha, self.v, self.rho) - get_Weight(y))*np.cos(self.alpha)+ get_dispan(y, self.alpha, self.v, self.rho)*np.sin(self.alpha)
@@ -29,31 +32,41 @@ class LoadCase:
     def x2_force(self, y: float) -> float:
         return -(get_Lspan(y, self.alpha, self.v, self.rho) - get_Weight(y))*np.sin(self.alpha)+ get_dispan(y, self.alpha, self.v, self.rho)*np.cos(self.alpha)
 
-    def z2_shear_diagram(self)->None:
+    def z2_shear_calc(self)->None:
         y_axis = np.linspace(0,11.98, 100)
         shear = []
         for y in y_axis:
             shear_val, _= sp.integrate.quad(self.z2_force, y, 11.98)
             shear.append(-shear_val)
 
-        plt.plot(y_axis, shear)
-        plt.title("Shear force diagram")
-        plt.ylabel("Shear Force [N]")
-        plt.xlabel("Spanwise location [m]")
-        plt.show()
+        self.z2_shear = sp.interpolate.p1d(y_axis, shear, kind="cubic", fill_value="extrapolate")
 
-    def x2_shear_diagram(self)->None:
+    def x2_shear_calc(self)->None:
         y_axis = np.linspace(0,11.98, 100)
         shear = []
         for y in y_axis:
             shear_val, _= sp.integrate.quad(self.x2_force, y, 11.98)
             shear.append(-shear_val)
+        
+        self.x2_shear = sp.interpolate.p1d(y_axis, shear, kind="cubic", fill_value="extrapolate")
 
-        plt.plot(y_axis, shear)
-        plt.title("Shear force diagram")
-        plt.ylabel("Shear Force [N]")
+    def z2_shear_diagram(self)-> None:
+        y_axis = np.linspace(0,11.98,100)
+        x_axis = self.z2_shear(y_axis)
+
+        plt.plot(y_axis, x_axis)
+        plt.ylabel("Shear force [N]")
         plt.xlabel("Spanwise location [m]")
-        plt.show()
+        plt.title("Vertical shearforce diagram")
+
+    def x2_shear_diagram(self)-> None:
+        y_axis = np.linspace(0,11.98,100)
+        x_axis = self.x2_shear(y_axis)
+
+        plt.plot(y_axis, x_axis)
+        plt.ylabel("Shear force [N]")
+        plt.xlabel("Spanwise location [m]")
+        plt.title("Horizontal shearforce diagram")
 
     def torque_diagram(self)-> None:
         torque_diagram(self.alpha, self.v, self.rho)
