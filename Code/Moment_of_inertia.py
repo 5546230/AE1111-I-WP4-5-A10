1d4e8023fceaecd8decb9097495258c1d84b802a
5546230 <n.g.j.schotmeijer@student.tudelft.nl>
2022-11-21 17:13:54 +0100

    centroid

--- Code/Moment_of_inertia.py
+++ Code/Moment_of_inertia.py
@@ -1,5 +1,5 @@
 import math
-
+import numpy as np
 c_r = 3.49 #[m]
 y = 0 #span
 c = c_r-c_r*(1-0.372)/12*y #formula for chord
@@ -24,12 +24,12 @@ A_2 = H*t
 A_3 = h_mid*t
 A_4 = H*t
 
-z_centroid = ((h_1-h/2)*A_3+h/2*A_1+h_low*A_4+A_2*(h-h_up/2))/(A_1+A_2+A_3+A_4)#(2*h_2/3*A_3+(h_2+h_mid/2)*A_2+(h_1+h_up/3)*A_1)/(A_1+A_2+A_3)
+z_centroid = ((h_1-h/2)*A_3+h/2*A_1+H/2*np.sin(beta_2)*A_4+A_2*(h-H/2*np.sin(beta_1)))/(A_1+A_2+A_3+A_4)#(2*h_2/3*A_3+(h_2+h_mid/2)*A_2+(h_1+h_up/3)*A_1)/(A_1+A_2+A_3)
 
-I_xx_1 = 1/12*t*h**3 + A_1*(h-z_centroid)**2
-I_xx_2 = 1/12*t*H**3*math.sin(beta_1)**2+A_2*(h-H/2*math.sin(beta_1)-z_centroid)**2
-I_xx_3 = 1/12*t*h_mid**3+A_3*(h_2+h_mid/2-z_centroid)**2
-I_xx_4 = 1/12*y*H**3*math.sin(beta_2)**2 + A_4*(z_centroid-H/2*math.sin(beta_2))**2
+I_xx_1 = 1/12*t*h**3 + A_1*(h/2-z_centroid)**2
+I_xx_2 = 1/12*t*H**3*math.sin(beta_1)**2+A_2*(h-H/2*np.sin(beta_1)-z_centroid)**2
+I_xx_3 = 1/12*t*h_mid**3+A_3*(h_1-h/2-z_centroid)**2
+I_xx_4 = 1/12*y*H**3*math.sin(beta_2)**2 + A_4*(z_centroid-H/2*np.sin(beta_2))**2
 
 
 
