576faf6aefe7de8b98b5f61fd7a97336d3b4b3eb
5546230 <n.g.j.schotmeijer@student.tudelft.nl>
2022-11-21 13:14:27 +0100

    shear diagram

--- Code/load_case.py
+++ Code/load_case.py
@@ -1,10 +1,11 @@
 from ISA import ISA_rho
 from interpolation import get_alpha, get_Lspan, get_dispan
+from Torque_diagram import torque_diagram
 from Weight_diagram import get_Weight
 import numpy as np
 import scipy as sp
-from Torque_diagram import torque_diagram
-from Shear_diagram import shear_diagram
+from matplotlib import pyplot as plt
+
 
 class LoadCase:
 
@@ -22,11 +23,40 @@ class LoadCase:
         self.cld = self.l/(0.5*self.rho*self.v**2*self.s)
         self.alpha = get_alpha(self.cld)
     
-    def shear_diagram(self)->None:
-        shear_diagram(self.alpha, self.v, self.rho)
+    def z2_force(self, y: float) -> float:
+        return (get_Lspan(y, self.alpha, self.v, self.rho) - get_Weight(y))*np.cos(self.alpha)+ get_dispan(y, self.alpha, self.v, self.rho)*np.sin(self.alpha)
+    
+    def x2_force(self, y: float) -> float:
+        return -(get_Lspan(y, self.alpha, self.v, self.rho) - get_Weight(y))*np.sin(self.alpha)+ get_dispan(y, self.alpha, self.v, self.rho)*np.cos(self.alpha)
+
+    def z2_shear_diagram(self)->None:
+        y_axis = np.linspace(0,11.98, 100)
+        shear = []
+        for y in y_axis:
+            shear_val, _= sp.integrate.quad(self.z2_force, y, 11.98)
+            shear.append(-shear_val)
+
+        plt.plot(y_axis, shear)
+        plt.title("Shear force diagram")
+        plt.ylabel("Shear Force [N]")
+        plt.xlabel("Spanwise location [m]")
+        plt.show()
+
+    def x2_shear_diagram(self)->None:
+        y_axis = np.linspace(0,11.98, 100)
+        shear = []
+        for y in y_axis:
+            shear_val, _= sp.integrate.quad(self.x2_force, y, 11.98)
+            shear.append(-shear_val)
+
+        plt.plot(y_axis, shear)
+        plt.title("Shear force diagram")
+        plt.ylabel("Shear Force [N]")
+        plt.xlabel("Spanwise location [m]")
+        plt.show()
 
     def torque_diagram(self)-> None:
         torque_diagram(self.alpha, self.v, self.rho)
 
 load1 = LoadCase(1, 162550, 200, 12500)
-load1.torque_diagram()
+load1.z2_shear_diagram()
