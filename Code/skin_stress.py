94cd35dae35f07f0a27706a0a592ed38adbb7f0d
Barnabas Tasi <barnab.tasi03@gmail.com>
2022-12-11 12:11:47 +0100

    Working on skin stresses

--- Code/skin_stress.py
+++ Code/skin_stress.py
@@ -1,2 +1,9 @@
+import numpy as np
+import scipy as sp
 from Moment_of_inertia import get_ixx
 from load_case import LoadCase
+from interpolation import *
+#test
+I = get_ixx(4, 2*10**-3)
+c = get_c(4)
+print(I, c)
