3a8bc96a64934db61f0fa90923892d634eea74e6
Barnabas Tasi <barnab.tasi03@gmail.com>
2022-12-20 00:47:13 +0100

    .

--- Code/col_buckling_testing.py
+++ Code/col_buckling_testing.py
@@ -27,7 +27,7 @@ print(option1.a)
 #this is insanely slow down here
 while not option1.test():
     ribs_list = np.array([0.38, 0.81, 1.26, 1.71, a, 6, 6.2, 6.24, 7.04, 7.74, 8.05])
-    a -= 0.01
+    a -= 0.1
     lengths = np.zeros(len(ribs_list))
     for i in range(0,len(ribs_list)):
         if i == 0:
