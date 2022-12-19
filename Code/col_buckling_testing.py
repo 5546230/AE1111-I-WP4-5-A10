69074255cf37cc247e993334ad107e87afcabc9f
Barnabas Tasi <barnab.tasi03@gmail.com>
2022-12-19 12:59:38 +0100

    .

--- Code/col_buckling_testing.py
+++ Code/col_buckling_testing.py
@@ -9,6 +9,13 @@ t_f = 5.5*10**-3 #m
 t_r = 4*10**-3 #m
 t_s = 9.3*10**-3
 
-ribs_list = np.array([0.63, 2.19, 2.61, 3.70, 4.00, 5.51, 5.34, 6.24, 7.04, 7.74, 8.05])
-option1 = design_option_column((m)*a_stringer, n, 10, ribs_list, t_s, t_f, t_r)
+ribs_list = np.array([0.63, 1.8, 2.61, 3.70, 4.00, 5.15, 5.34, 6.24, 7.04, 7.74, 8.05])
+lengths = np.zeros(len(ribs_list))
+for i in range(0,len(ribs_list)):
+    if i == 0:
+        lengths[i] = ribs_list[i]
+    else:
+        lengths[i] = ribs_list[i]-ribs_list[i-1]
+print(lengths)
+option1 = design_option_column((m)*a_stringer, n, 10, lengths, t_s, t_f, t_r)
 option1.generate_plot()
