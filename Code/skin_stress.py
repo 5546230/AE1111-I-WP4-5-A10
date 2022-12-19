f840395c6dd87ca1ca49c60332381874f89ce95c
5546230 <n.g.j.schotmeijer@student.tudelft.nl>
2022-12-19 22:31:47 +0100

    improved iteration

--- Code/skin_stress.py
+++ Code/skin_stress.py
@@ -100,7 +100,7 @@ if __name__=="__main__":
     ########## Rib placement ##########
     # iterated thickness and stringer area carries through
     dt = 0.0001
-    dm = 0.02
+    dm = 0.001
     dy = 0.01 #m
 
     #t is set
@@ -148,21 +148,32 @@ if __name__=="__main__":
             #     s = skin_stress(y1_0, t_f, t_r, t, a_stringer, load_max_compr, n, m)
 
             ##########
+            m_prev = m
+            n_prev = n
 
             while t < 0.02:
+                m=1
+                n=16
                 #check if amount of stringers can be reduced
                 while design_option_compr(m*a_stringer, n-2, 10, t_f, t_r, t, 1, load_max_compr).test():
                     if n-2>=0:
                         n-=2
                     else:
                         break
+
+                #speed up computation
+                if n == n_prev:
+                    m = m_prev
+
                 #check if m can be reduced
-                for multiplier in range(int(m/dm),0):
-                    if design_option_compr((m-multiplier*dm)*a_stringer, n, 10, t_f, t_r, t, 1, load_max_compr).test():
-                        m-=dm*multiplier
+                while design_option_compr((m-dm)*a_stringer, n, 10, t_f, t_r, t, 1, load_max_compr).test():
+                    if m-2*dm>1e-3 and n>0:
+                        m-=dm
+                    else:
                         break
-
-
+                n_prev = n
+                m_prev = m	
+                print(n, m, t)  
                 y1 = y1_0 #m
                 y2 = y1 + dy #m
                 while y2 < 8:
