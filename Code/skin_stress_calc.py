5cf9a1ea8da113fa53c20d60cd42c7f518be257d
Barnabas Tasi <barnab.tasi03@gmail.com>
2022-12-21 16:07:34 +0100

    Update skin_stress_calc.py

--- Code/skin_stress_calc.py
+++ Code/skin_stress_calc.py
@@ -4,14 +4,19 @@ from load_case import *
 
 t_f = 5.5*10**-3 #m
 t_r = 4*10**-3 #m
-t_s = 9.3*10**-3 #m (skin thickness - ask for it in each bay)
+t_s = 13.6*10**-3 #m (skin thickness - ask for it in each bay)
 a_stringer = (35*10**-3)**2
 load_max_compr = LoadCase(2.62*1.5, 16575.6*9.80665, 250.79, 12500)
 n = 4
 m = 0.08
 
 #INPUT
-y = 0.35
+y1 = 0.125
 
-stress = skin_stress(y, t_f, t_r, t_s, a_stringer, load_max_compr, n, m)
-print(stress)
+stress1 = skin_stress(y1, t_f, t_r, t_s, a_stringer, load_max_compr, n, m)
+print(stress1)
+
+y2 = 0.7
+
+stress2 = skin_stress(y2, t_f, t_r, t_s, a_stringer, load_max_compr, n, m)
+print(stress2)
